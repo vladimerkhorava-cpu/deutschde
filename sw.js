17836d5040a8cbca7123b36e7e4ed7e1b20e5c33
vladimerkhorava-cpu <vladimer.khorava@gmail.com>
2026-03-13 12:38:21 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// v2 - force update
+const CACHE = 'deutschde-v2';
+const ASSETS = ['/', '/index.html', '/app.js', '/style.css', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
